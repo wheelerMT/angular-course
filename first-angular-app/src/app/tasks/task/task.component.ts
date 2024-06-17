42b4f95fc193bcbee0d720a90e59616ce35c8b40
Matt Wheeler <wheelermt@proton.me>
2024-06-17 20:13:55 +0100

    Add dynamic data to task component

--- first-angular-app/src/app/tasks/task/task.component.ts
+++ first-angular-app/src/app/tasks/task/task.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { type Task } from './task.model';
 
 @Component({
   selector: 'app-task',
   standalone: true,
   imports: [],
   templateUrl: './task.component.html',
-  styleUrl: './task.component.css'
+  styleUrl: './task.component.css',
 })
 export class TaskComponent {
-
+  task = input.required<Task>();
 }
